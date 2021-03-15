83f6a4632f57b98c247fc83aed9de35bb62fd0dc
tomaszgrzywacz92 <tomaszgrzywacz92@gmail.com>
2021-03-15 15:58:15 +0100

    16. Tworzenie encji

--- Controllers/RestaurantController.cs
+++ Controllers/RestaurantController.cs
@@ -22,6 +22,17 @@ namespace api.Controllers
             _mapper = mapper;
         }
 
+        [HttpPost]
+        public ActionResult CreateRestaurant([FromBody] CreateRestaurantDto dto)
+        {
+            var restaurant = _mapper.Map<Restaurant>(dto);
+
+            _dbContext.Restaurants.Add(restaurant);
+            _dbContext.SaveChanges();
+
+            return Created($"/api/restaurant/{restaurant.Id}", null);
+        }
+
         [HttpGet]
         public ActionResult<IEnumerable<RestaurantDto>> GetAll()
         {
