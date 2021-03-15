1102770e84dca02dfee900bdbf74042293d2e26d
tomaszgrzywacz92 <tomaszgrzywacz92@gmail.com>
2021-03-15 15:42:09 +0100

    15. Mapowanie do modeli Dto

---
+++ RestaurantMappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using api.Entities;
+using api.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api
+{
+    public class RestaurantMappingProfile : Profile
+    {
+        public RestaurantMappingProfile()
+        {
+            CreateMap<Restaurant, RestaurantDto>()
+                .ForMember(m => m.City, c => c.MapFrom(s => s.Address.City))
+                .ForMember(m => m.Street, c => c.MapFrom(s => s.Address.Street))
+                .ForMember(m => m.PostalCode, c => c.MapFrom(s => s.Address.PostalCode));
+
+            CreateMap<Dish, DishDto>();
+        }
+    }
+}
