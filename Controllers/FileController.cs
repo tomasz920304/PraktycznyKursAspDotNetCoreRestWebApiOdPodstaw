e6a1d5b95fc683de91f0b531b7927fbb3af20fd7
tomaszgrzywacz92 <tomaszgrzywacz92@gmail.com>
2021-03-17 16:30:18 +0100

    52. Cache odpowiedzi

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -16,6 +16,7 @@ namespace RestaurantAPI.Controllers
     public class FileController : ControllerBase
     {
         [HttpGet]
+        [ResponseCache(Duration = 1200, VaryByQueryKeys = new[] { "fileName" })]
         public ActionResult GetFile([FromQuery] string fileName)
         {
             var rootPath = Directory.GetCurrentDirectory();
